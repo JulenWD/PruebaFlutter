43f160b4a55d6463625070098e9f0820a2fc3a2c
Julenalonso007 <julenmj@hotmail.es>
2025-07-04 08:45:16 +0200

    Primer commit desde carpeta aislada

---
+++ lib/screens/imc_home_screen.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:imc_calculator/components/gender_selector.dart';
+import 'package:imc_calculator/components/height_selector.dart';
+import 'package:imc_calculator/components/number_selector.dart';
+
+class ImcHomeScreen extends StatefulWidget {
+  const ImcHomeScreen({super.key});
+
+  @override
+  State<ImcHomeScreen> createState() => _ImcHomeScreenState();
+}
+
+class _ImcHomeScreenState extends State<ImcHomeScreen> {
+  int selectedAge = 30;
+  int selectedWeight = 70;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        GenderSelector(),
+        HeightSelector(),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Row(
+            children: [
+              Expanded(
+                child: NumberSelector(
+                  title: "PESO",
+                  value: selectedWeight,
+                  onIncrement: () {
+                    setState(() {
+                      selectedWeight++;
+                    });
+                  },
+                  onDecrement: () {
+                    setState(() {
+                      if (selectedWeight > 0) {
+                        selectedWeight--;
+                      }
+                    });
+                  },
+                ),
+              ),
+              SizedBox(width: 16),
+              Expanded(
+                child: NumberSelector(
+                  title: "EDAD",
+                  value: selectedAge,
+                  onIncrement: () {
+                    setState(() {
+                      selectedAge++;
+                    });
+                  },
+                  onDecrement: () {
+                    setState(() {
+                      if (selectedAge > 0) {
+                        selectedAge--;
+                      }
+                    });
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
